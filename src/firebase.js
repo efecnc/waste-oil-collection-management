93168f6165bae690ed16e25c060c4cb09d33764e
efecnc <efecnc@users.noreply.github.com>
2025-01-09 15:45:15 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCAHRbOt-EXAMPLE",
-  authDomain: "depo-stok-31ca2.firebaseapp.com",
-  projectId: "depo-stok-31ca2",
-  storageBucket: "depo-stok-31ca2.appspot.com",
-  messagingSenderId: "603516286341",
-  appId: "1:603516286341:web:e4fc702b4cd45bbcf140ec",
-  measurementId: "G-P2B1B3X1EL"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
